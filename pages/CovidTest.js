d408c2f8dfa13c015e32a24e825cc1c3f83c75b2
dreampotential <aaron@dreampotential.org>
2021-10-22 00:07:40 +0800

    Added Covid Testing Page

--- pages/CovidTest.js
+++ pages/CovidTest.js
@@ -1,3 +1,9 @@
+import Image from 'next/image'
+import deposit_img from '../Images/covid/deposit.png'
+import advantage1_img from '../Images/covid/advantage1.png'
+import advantage2_img from '../Images/covid/advantage2.png'
+import advantage3_img from '../Images/covid/advantage3.png'
+
 const Covidtest = () => {
     return (
         <div>
@@ -11,14 +17,16 @@ const Covidtest = () => {
                 </div>
             </div>
 
-            <div class="section section--form">
-                <div class="container container--form">
-                    <div class="form-text">
+            <div className="section section--form">
+                <div className="container container--form">
+                    <div className="form-text">
                         <p>Our customized solution will facilitate the safe reopening of your workplace or campus. Collect proof of vaccination and test track your staff/students with verification uploaded by your personal.</p>
                     </div>
-                    <div class="form-image"></div>
-                    <div class="form-container">
-                        <div class="form__title">Get in touch</div>
+                    <div className="form-image">
+                        <Image src = {deposit_img} layout="responsive" objectFit="contain" quality={100} />
+                    </div>
+                    <div className="form-container">
+                        <div className="form__title">Get in touch</div>
                         <form action="/ContactSubmission" method="post">
                             <input type="text" name="your-name" placeholder="Your Full Name" />
                             <input type="email" name="your-email" placeholder="Email Address" />
@@ -29,6 +37,41 @@ const Covidtest = () => {
                     </div>
                 </div>
             </div>
+
+            <div className="section section--advantages">
+                <div className="container container--advantages">
+                    <div className="advantage advantage--one">
+                        <div className="advantage__icon">
+                            <Image src = {advantage1_img} />
+                        </div>
+                        <div className = "advantage_title">
+                            Fully Automated
+                        </div>
+                        <div className="advantage__description">Our proprietary technology will simplify the verification review process.</div>
+                    </div>
+
+                    <div className="advantage advantage--two">
+                        <div className="advantage__icon">
+                            <Image src = {advantage2_img} />
+                        </div>
+                        <div className = "advantage_title">
+                            Easy Verification
+                        </div>
+                        <div className="advantage__description">Staff and students will receive a text and email request every 7 days to update their vaccination status.</div>
+                    </div>
+
+                    <div className="advantage advantage--three">
+                        <div className="advantage__icon">
+                            <Image src = {advantage3_img} />
+                        </div>
+                        <div className = "advantage_title">
+                            HIPAA-compliant
+                        </div>
+                        <div className="advantage__description">Get the ultimate control with 1-click validation of the results by your administration team.</div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 }
