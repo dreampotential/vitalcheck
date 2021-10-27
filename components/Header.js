dc37a7d25a3fce47f26c704ce872be906dee4b6d
dreampotential <aaron@dreampotential.org>
2021-10-28 04:46:48 +0800

    more updates

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,11 @@
 import React from "react";
+import Image from 'next/image'
+import logo from '../Images/footer/logo.png'
 const Header = () => {
     return (
-        <div className = "header">This is the header section</div>
+        <div className = "header">
+            <Image src = {logo} width = {210} height = {70} />
+        </div>
     );
 }
 export default Header;
