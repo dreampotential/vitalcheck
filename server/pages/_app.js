e84ba6a91404db67cd977d4fb39fbaff4d6a58f8
Aaron Rosen <aaronorosen@gmail.com>
2021-10-28 14:38:55 -0700

    Updates

---
+++ server/pages/_app.js
@@ -0,0 +1,358 @@
+(() => {
+var exports = {};
+exports.id = 888;
+exports.ids = [888];
+exports.modules = {
+
+/***/ 943:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ _app)
+});
+
+// EXTERNAL MODULE: ./node_modules/next/image.js
+var next_image = __webpack_require__(675);
+;// CONCATENATED MODULE: ./Images/footer/footer_logo.png
+/* harmony default export */ const footer_logo = ({"src":"/_next/static/image/Images/footer/footer_logo.ba86dd413464a7676ade106a2f930e83.png","height":210,"width":630,"blurDataURL":"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAADCAYAAACuyE5IAAAATElEQVR42mP4tmuTwb8fP7T///+v/O/fPz0g1oTSqiCa4VNTtgRQUgzIkQNiGSDmg9KSIJoBKAlSqQOklYFYCchWA2ItINb99+8fIwAXLEjyaTUlUwAAAABJRU5ErkJggg=="});
+;// CONCATENATED MODULE: ./Images/footer/site_footer_logo.png
+/* harmony default export */ const site_footer_logo = ({"src":"/_next/static/image/Images/footer/site_footer_logo.f374b094cb270b8e4c635d07906b94c8.png","height":194,"width":300,"blurDataURL":"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAFCAYAAAB4ka1VAAAAjklEQVR42mMAgY7l65kfPX8ufuf5MzYGdPD//3+uN69eHXj88NHnZ0+fXltw/nXFxItfshpPf8x13PNWnOHM9UfCVx+8+n3q9sv/Z+68+D/7/Lv/865++T/94qf/QfvfOjAwMKwU3X7m/uzz91/+33/t+SaGedfd0k981g3b98bQYdcrTrhVbx/c0GXAAgB9pU5WBSLCBAAAAABJRU5ErkJggg=="});
+// EXTERNAL MODULE: ./node_modules/next/link.js
+var next_link = __webpack_require__(664);
+// EXTERNAL MODULE: external "react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(282);
+;// CONCATENATED MODULE: ./components/Footer.js
+
+
+
+
+
+
+
+const Footer = () => {
+  return /*#__PURE__*/jsx_runtime_.jsx("div", {
+    className: "main_footer",
+    children: /*#__PURE__*/(0,jsx_runtime_.jsxs)("div", {
+      className: "footer_body",
+      children: [/*#__PURE__*/(0,jsx_runtime_.jsxs)("div", {
+        className: "footer_top",
+        children: [/*#__PURE__*/jsx_runtime_.jsx("div", {
+          className: "footer_img",
+          children: /*#__PURE__*/jsx_runtime_.jsx(next_image.default, {
+            src: footer_logo,
+            width: 210,
+            height: 70
+          })
+        }), /*#__PURE__*/(0,jsx_runtime_.jsxs)("div", {
+          className: "footer_contact",
+          children: [/*#__PURE__*/(0,jsx_runtime_.jsxs)("div", {
+            className: "contact1",
+            children: [/*#__PURE__*/jsx_runtime_.jsx("p", {
+              children: /*#__PURE__*/jsx_runtime_.jsx("strong", {
+                children: "Klarion Wellness Inc.,"
+              })
+            }), /*#__PURE__*/jsx_runtime_.jsx("p", {
+              children: /*#__PURE__*/jsx_runtime_.jsx("strong", {
+                children: "Administrative Agent"
+              })
+            }), /*#__PURE__*/jsx_runtime_.jsx("p", {
+              children: "201 West 37th Street"
+            }), /*#__PURE__*/jsx_runtime_.jsx("p", {
+              children: "Suite 402"
+            }), /*#__PURE__*/jsx_runtime_.jsx("p", {
+              children: "New York, NY 10018"
+            })]
+          }), /*#__PURE__*/(0,jsx_runtime_.jsxs)("div", {
+            className: "contact2",
+            children: [/*#__PURE__*/jsx_runtime_.jsx("p", {
+              children: /*#__PURE__*/jsx_runtime_.jsx("strong", {
+                children: "ED Medical, P.C."
+              })
+            }), /*#__PURE__*/jsx_runtime_.jsx("p", {
+              children: "140 Grand Avenue,"
+            }), /*#__PURE__*/jsx_runtime_.jsx("p", {
+              children: "Englewood, NJ 07631"
+            })]
+          }), /*#__PURE__*/(0,jsx_runtime_.jsxs)("div", {
+            className: "contact3",
+            children: [/*#__PURE__*/jsx_runtime_.jsx("p", {
+              children: "Phone: 646. 450. 7751"
+            }), /*#__PURE__*/(0,jsx_runtime_.jsxs)("p", {
+              children: ["Email: ", /*#__PURE__*/jsx_runtime_.jsx("span", {
+                children: /*#__PURE__*/jsx_runtime_.jsx("a", {
+                  href: "mailto:info@getvitalcheck.com",
+                  children: "info@getvitalcheck.com"
+                })
+              })]
+            })]
+          })]
+        }), /*#__PURE__*/(0,jsx_runtime_.jsxs)("div", {
+          className: "footer_logo",
+          children: [/*#__PURE__*/jsx_runtime_.jsx("div", {
+            className: "footer_text",
+            children: "The VitalCheck-Doctors In Your Office Program (the \u201CVitalCheck Program\u201D) is a collaboration between ED Medical, P.C., a New Jersey based medical practice, and Klarion Wellness, Inc., a company located in New York, New York, designed to bring high-quality preventive healthcare to employees at their place of work or via telemedicine. Under the VitalCheck Program board certified physicians conduct wellness and other preventive medical examinations, including back to work screening for COVID-19, and provide related preventive health services, . ED Medical, P.C. oversees the medical services provided under the Program. Klarion Wellness is the administrative agent for the Program and provides administrative services and proprietary technology to the Program."
+          }), /*#__PURE__*/jsx_runtime_.jsx("div", {
+            className: "footer_site_logo",
+            children: /*#__PURE__*/jsx_runtime_.jsx(next_image.default, {
+              src: site_footer_logo,
+              width: 205,
+              height: 132
+            })
+          })]
+        })]
+      }), /*#__PURE__*/(0,jsx_runtime_.jsxs)("div", {
+        className: "footer_bottom",
+        children: [/*#__PURE__*/(0,jsx_runtime_.jsxs)("div", {
+          className: "footer_privacy",
+          children: [/*#__PURE__*/jsx_runtime_.jsx(next_link.default, {
+            href: "/",
+            children: "Terms of Service"
+          }), /*#__PURE__*/jsx_runtime_.jsx(next_link.default, {
+            href: "/",
+            children: "Privacy Policy"
+          }), /*#__PURE__*/jsx_runtime_.jsx(next_link.default, {
+            href: "/",
+            children: "Notice of Privacy"
+          })]
+        }), /*#__PURE__*/jsx_runtime_.jsx("div", {
+          className: "footer_copy",
+          children: /*#__PURE__*/jsx_runtime_.jsx("p", {
+            children: "VitalCheck-Doctors In Your Office \xA9 2020"
+          })
+        })]
+      })]
+    })
+  });
+};
+
+/* harmony default export */ const components_Footer = (Footer);
+// EXTERNAL MODULE: external "react"
+var external_react_ = __webpack_require__(297);
+;// CONCATENATED MODULE: ./Images/footer/logo.png
+/* harmony default export */ const logo = ({"src":"/_next/static/image/Images/footer/logo.a148fa9e8210f2902fb367eb37894f65.png","height":391,"width":1169,"blurDataURL":"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAADCAYAAACuyE5IAAAAa0lEQVR4nGPc45Ni2vta8/e1Pyw/+Rn/c/5nYPjJwMDACcTf/v9n4GJ8HcggJHq457+G8D/2H4yM/x88/PlZVoaN+8+f///5+VmZGXkj1ih7MP9iZmBk+gXU9f/fv39MjIyMbEA2s5KyzA0A26Mk3TVyUhcAAAAASUVORK5CYII="});
+;// CONCATENATED MODULE: ./components/Header.js
+
+
+
+
+
+
+
+const Header = () => {
+  return /*#__PURE__*/jsx_runtime_.jsx("div", {
+    className: "header",
+    children: /*#__PURE__*/(0,jsx_runtime_.jsxs)("div", {
+      className: "header_container",
+      children: [/*#__PURE__*/jsx_runtime_.jsx(next_image.default, {
+        src: logo,
+        width: 210,
+        height: 70
+      }), /*#__PURE__*/jsx_runtime_.jsx(next_link.default, {
+        href: "/request_info",
+        children: /*#__PURE__*/jsx_runtime_.jsx("a", {
+          className: "header_more_info",
+          children: "Request More Information"
+        })
+      })]
+    })
+  });
+};
+
+/* harmony default export */ const components_Header = (Header);
+;// CONCATENATED MODULE: ./components/Layout.js
+
+
+
+
+
+const Layout = props => /*#__PURE__*/(0,jsx_runtime_.jsxs)("div", {
+  children: [/*#__PURE__*/jsx_runtime_.jsx(components_Header, {}), props.children, /*#__PURE__*/jsx_runtime_.jsx(components_Footer, {})]
+});
+
+/* harmony default export */ const components_Layout = (Layout);
+;// CONCATENATED MODULE: ./pages/_app.js
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) { symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); } keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function MyApp({
+  Component,
+  pageProps
+}) {
+  return /*#__PURE__*/jsx_runtime_.jsx(components_Layout, {
+    children: /*#__PURE__*/jsx_runtime_.jsx(Component, _objectSpread({}, pageProps))
+  });
+}
+
+/* harmony default export */ const _app = (MyApp);
+
+/***/ }),
+
+/***/ 325:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/server/denormalize-page-path.js");
+
+/***/ }),
+
+/***/ 822:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/server/image-config.js");
+
+/***/ }),
+
+/***/ 695:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/head.js");
+
+/***/ }),
+
+/***/ 378:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/i18n/normalize-locale-path.js");
+
+/***/ }),
+
+/***/ 162:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/mitt.js");
+
+/***/ }),
+
+/***/ 773:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router-context.js");
+
+/***/ }),
+
+/***/ 248:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/get-asset-path-from-route.js");
+
+/***/ }),
+
+/***/ 372:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/is-dynamic.js");
+
+/***/ }),
+
+/***/ 665:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/parse-relative-url.js");
+
+/***/ }),
+
+/***/ 747:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/querystring.js");
+
+/***/ }),
+
+/***/ 333:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/route-matcher.js");
+
+/***/ }),
+
+/***/ 456:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/route-regex.js");
+
+/***/ }),
+
+/***/ 556:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/to-base-64.js");
+
+/***/ }),
+
+/***/ 620:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/utils.js");
+
+/***/ }),
+
+/***/ 297:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("react");
+
+/***/ }),
+
+/***/ 282:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("react/jsx-runtime");
+
+/***/ }),
+
+/***/ 431:
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [675,664], () => (__webpack_exec__(943)));
+module.exports = __webpack_exports__;
+
+})();
