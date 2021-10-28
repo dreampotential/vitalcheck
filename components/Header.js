6ef9544ec40f5d0a773767aeb18801785f2c9955
dreampotential <aaron@dreampotential.org>
2021-10-28 23:00:52 +0800

    Updated Header

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Image from 'next/image'
 import logo from '../Images/footer/logo.png'
+import Link from 'next/link'
+
 const Header = () => {
     return (
         <div className = "header">
-            <Image src = {logo} width = {210} height = {70} />
+            <div className = "header_container">
+                <Image src = {logo} width = {210} height = {70} />
+                <Link href = "/request_info"><a className = "header_more_info">Request More Information</a></Link>
+            </div>
         </div>
     );
 }
